78b64ecd38e3b409f5d30292a2d4e57cbd8241cf
bmpss92295 <111870157+bmpss92295@users.noreply.github.com>
2023-03-13 11:45:32 +0800

    Initial commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class 即時股價機器人Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = '即時股價機器人'
---
+++ views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+import now_price
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                stock_price = now_price.yahoo_stock_now(event.message.text)
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=stock_price))
+                
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
